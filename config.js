73c5bdc2f1f88a686e93793ccb75a80395d8de83
cloudcorp30-bera <cloudcorp30@gmail.com>
2026-03-13 17:35:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || '8648971888:AAE0VoGdrjrAtNNUYOuMlilaK8f3vKzlPcA',
     PANEL_DOMAIN: process.env.PANEL_DOMAIN || 'https://bera-panel-production.up.railway.app',
-    PANEL_API_KEY: process.env.PANEL_API_KEY || 'ptla_8gN5bQmWGHaOKdDZY8uqGlEC8InTE0MtubBj3ZgQ4Tn',
+    PANEL_API_KEY: process.env.PANEL_API_KEY || 'ptla_EW6B5h4UHJYKd0L3M1OnRuv8IBYpHszuaqSxXkPe3II',
     AUTHORIZED_ADMINS: (process.env.AUTHORIZED_ADMINS || '7038158483').split(',').map(Number),
     GLOBAL_EGG: process.env.GLOBAL_EGG || '15',
     GLOBAL_LOCATION: process.env.GLOBAL_LOCATION || '1'
